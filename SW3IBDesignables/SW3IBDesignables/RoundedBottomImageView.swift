126f0ea3e11eb4097f191e78b056e04838c20f3b
DonMag <ACP@ACPs-iMac.local>
2017-02-27 10:59:10 -0500

    First Look - Swift 3

---
+++ SW3IBDesignables/SW3IBDesignables/RoundedBottomImageView.swift
@@ -0,0 +1,67 @@
+//
+//  RoundedBottomImageView.swift
+//  SW3IBDesign
+//
+//  Created by Don Mag on 2/27/17.
+//  Copyright © 2017 DonMag. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class RoundedBottomImageView: UIView {
+
+	var imageView: UIImageView!
+	
+	@IBInspectable var image: UIImage? {
+		didSet { self.imageView.image = image }
+	}
+	
+	@IBInspectable var roundingValue: CGFloat = 0.0 {
+		didSet {
+			self.setNeedsLayout()
+		}
+	}
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		doMyInit()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+		doMyInit()
+	}
+	
+	func doMyInit() {
+		
+		imageView = UIImageView()
+		imageView.backgroundColor = UIColor.red
+		imageView.contentMode = UIViewContentMode.scaleToFill
+		addSubview(imageView)
+		
+	}
+	
+	override func layoutSubviews() {
+		super.layoutSubviews()
+		imageView.frame = self.bounds
+
+		let rect = self.bounds
+		let y:CGFloat = rect.size.height - roundingValue
+		let curveTo:CGFloat = rect.size.height
+		
+		let myBezier = UIBezierPath()
+		myBezier.move(to: CGPoint(x: 0, y: y))
+		myBezier.addQuadCurve(to: CGPoint(x: rect.width, y: y), controlPoint: CGPoint(x: rect.width / 2, y: curveTo))
+		myBezier.addLine(to: CGPoint(x: rect.width, y: 0))
+		myBezier.addLine(to: CGPoint(x: 0, y: 0))
+		myBezier.close()
+		
+		let maskForPath = CAShapeLayer()
+		maskForPath.path = myBezier.cgPath
+		layer.mask = maskForPath
+		
+	}
+
+}
---
+++ SW3IBDesignables/SW3IBDesignables/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SW3IBDesignables
+//
+//  Created by Don Mag on 2/27/17.
+//  Copyright © 2017 DonMag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
